47ffe2d3678459751d62f63e6de83818657dba47
James <jtronlabs@gmail.com>
2017-11-08 10:25:49 -0500

    added lots of info to data. Removed some default stuff from create-preact-app

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,9 @@ import { Router } from 'preact-router';
 
 import Header from './header';
 import Home from '../routes/home';
-import Profile from '../routes/profile';
-// import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
@@ -22,8 +20,6 @@ export default class App extends Component {
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,63 +13,20 @@ import 'preact-material-components/Toolbar/style.css';
 import style from './style';
 
 export default class Header extends Component {
-	closeDrawer(){
-		this.drawer.MDComponent.open = false;
-		this.state = {
-			darkThemeEnabled: false
-		};
-	}
 	render() {
 		return (
 			<div>
 				<Toolbar className="toolbar">
 					<Toolbar.Row>
 						<Toolbar.Section align-start={true}>
-							<Toolbar.Icon menu={true} onClick={() => {
-								this.drawer.MDComponent.open = true;
-							}}>menu</Toolbar.Icon>
 							<Toolbar.Title>
 								Preact app
 							</Toolbar.Title>
 						</Toolbar.Section>
-						<Toolbar.Section align-end={true} onClick={()=>{
-							this.dialog.MDComponent.show();
-						}}>
-							<Toolbar.Icon>settings</Toolbar.Icon>
-						</Toolbar.Section>
 					</Toolbar.Row>
 				</Toolbar>
-				<Drawer.TemporaryDrawer ref={drawer=>{this.drawer = drawer;}} >
-					<Drawer.TemporaryDrawerContent>
-						<List>
-							<List.LinkItem onClick={()=>{route('/'); this.closeDrawer();}}>
-								<List.ItemIcon>home</List.ItemIcon>
-									Home
-							</List.LinkItem>
-							<List.LinkItem onClick={()=>{route('/profile'); this.closeDrawer();}}>
-								<List.ItemIcon>account_circle</List.ItemIcon>
-									Profile
-							</List.LinkItem>
-						</List>
-					</Drawer.TemporaryDrawerContent>
-				</Drawer.TemporaryDrawer>
 				<Dialog ref={dialog=>{this.dialog=dialog;}}>
           <Dialog.Header>Settings</Dialog.Header>
-          <Dialog.Body>
-						<div>
-							Enable dark theme <Switch onClick={()=>{
-								this.setState({
-									darkThemeEnabled: !this.state.darkThemeEnabled
-								},() => {
-									if(this.state.darkThemeEnabled) {
-										document.body.classList.add('mdc-theme--dark');
-									} else {
-										document.body.classList.remove('mdc-theme--dark');
-									}
-								});
-							}}/>
-						</div>
-          </Dialog.Body>
           <Dialog.Footer>
             <Dialog.FooterButton accept={true}>okay</Dialog.FooterButton>
           </Dialog.Footer>
