a3895e0c3b6269fe03d488537d0cc6453225608a
James <jtronlabs@gmail.com>
2017-11-09 10:56:14 -0500

    working search

--- src/components/studies/index.jsx
+++ src/components/studies/index.jsx
@@ -4,22 +4,42 @@ import Study from '../study/index.jsx';
 
 export default class Studies extends Component {
 	static preprocessStudies(studies) {
-		return studies.map((study) => {
+		return studies.map((study, indx) => {
 			let tags = study.tags;
 			study.tags = tags.split(',');
+			study.id = indx;
 			return study;
 		});
 	}
+
+	static getAllTags(studies){
+		let tags = new Set();
+		for (const study of studies){
+			for (const tag of study.tags){
+				tags.add(tag);
+			}
+		}
+		return Array.from(tags);
+	}
+
 	render() {
-		let studies = Studies.preprocessStudies(this.props.studies);
-		let studyComponents = studies.map((research, indx) =>
-			(
-				<Study
-					study={research}
-					key={indx}
-				/>
-			)
-		);
+		let studies = Studies.preprocessStudies(this.props.research);
+		const approvedTags = Studies.getAllTags(studies);
+		let studyComponents = studies.reduce((allStudies, study) => {
+			//filter out un-approved studies
+			for (const tag of study.tags){
+				if (approvedTags.includes(tag)){
+					allStudies.push(
+						<Study
+							study={study}
+							key={study.id}
+						/>
+					);
+					break;
+				}
+			}
+			return allStudies;
+		}, []);
 
 		return (
 			<div>
--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,27 +1,13 @@
 import { h, Component } from 'preact';
-import style from './style';
-import Study from '../../components/study/index.jsx';
-
+import Studies from '../../components/studies/index.jsx';
 import health from '../../assets/data/health.json';
 
 export default class Home extends Component {
-	static preprocessStudies(studies) {
-		return studies.map((study) => {
-			let tags = study.tags;
-			study.tags = tags.split(',');
-			return study;
-		});
-	}
 	render() {
-		let studies = Home.preprocessStudies(health);
-		let studyComponents = studies.map((research) =>
-			<Study study={research} />
-		);
-
 		return (
-			<div class={style.home}>
-				{studyComponents}
-			</div>
+			<Studies
+				research={health}
+			/>
 		);
 	}
 }
