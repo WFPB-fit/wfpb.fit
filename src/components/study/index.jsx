e990879a6051200d2d4941cfe5982f844c7a70ba
James <jtronlabs@gmail.com>
2017-11-08 23:45:37 -0500

    removed titleize package

--- src/components/study/index.jsx
+++ src/components/study/index.jsx
@@ -1,31 +1,37 @@
 import { h, Component } from 'preact';
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
-import 'preact-material-components/Button/style.css';
 import style from './style';
-import titleize from 'titleize';
 
 export default class Study extends Component {
+	static titleize(string) {
+		let arr = string.split(' ');
+		arr = arr.map(Study.capitalize);
+		return arr.join(' ');
+	}
+	static capitalize(string){
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 	render() {
 		const study = this.props.study;
 
 		let pdf = '';
-		if (study.pdf){
+		if (study.pdf) {
 			pdf = `- <a href=${study.url}>PDF</a>`;
 		}
-		let study_type = 'Study';
-		if (study.is_meta){
-			study_type = `Meta Analysis`;
+		let studyType = 'Study';
+		if (study.is_meta) {
+			studyType = `Meta Analysis`;
 		}
 		let tags = study.tags.split(',').join(', ');
-		tags = titleize(tags);
+		tags = Study.titleize(tags);
 
 		return (
 			<div class={style.home}>
 				<Card>
 					<Card.Primary>
-						<Card.Title><a href={`${study.url}`}>{titleize(study.title)}</a></Card.Title>
-						<Card.Subtitle>{study.year} - {titleize(study.availability)} - {study_type} {pdf} - {tags}</Card.Subtitle>
+						<Card.Title><a href={`${study.url}`}>{Study.titleize(study.title)}</a></Card.Title>
+						<Card.Subtitle>{study.year} - {Study.titleize(study.availability)} - {studyType} {pdf} - {tags}</Card.Subtitle>
 					</Card.Primary>
 					<Card.SupportingText>
 						<blockquote>{study.quote}</blockquote>
