e4e385fa8dfed69dd79737c1fce5805bf584446a
James <jtronlabs@gmail.com>
2017-11-29 15:29:46 -0500

    fixed bugs in SQL that were allowing supposedly removed foods to pass through

--- src/utils/fdaDataCreation/selections.sql
+++ src/utils/fdaDataCreation/selections.sql
@@ -4,49 +4,58 @@
 SELECT food.id,food.long_desc,food_group.name FROM food
 	inner join food_group on food_group.id=food.food_group_id
 	where
-	LENGTH(long_desc) < 11 or --short named foods are hard to filter for in other metrics and can be popular
+	--------- TOTAL FOOD GROUP REMOVAL ---------
 	(
-		--------- GENERAL FILTERING ---------
-		( -- Remove specific food groups first
-			food.food_group_id !=300 and -- baby food
-			food.food_group_id !=600 and -- soups/sauces
-			food.food_group_id !=3500 and -- native food
-			food.food_group_id !=3600 and --restaruants
-			food.food_group_id != 1800 --baked products
-		) and
+		food.food_group_id !=300 and -- baby food
+		food.food_group_id !=600 and -- soups/sauces
+		food.food_group_id !=3500 and -- native food
+		food.food_group_id !=3600 and --restaruants
+		food.food_group_id != 1800 --baked products
+	) and
+	--------- GENERAL FILTERING ---------
+	(
 		manufac_name is '' and --remove company specific food
-		long_desc not like '%by-products%' and --remove waste food
+		long_desc not like '%by-product%' and --remove waste food
+		long_desc not like '%separable lean and fat%' and --most people cut off the pure fat from their steaks/meat, so we won't include it
 		long_desc not like '%imported%' and -- remove imported food
 		long_desc not like '%industrial%' and -- this is a consumer facing app
-		long_desc like '%USDA Commodity%' or --rare and may be useful, maybe not
-
-		--------- PRESERVATIVES & ADDITIVES ---------
-		-- Do not include foods with additives. This is a way to limit data transfer/'paradox of choice', and
-		-- is really giving processed foods a better nutrient profile than they deserve. So it's a forgiving choice towards processed foods
+		long_desc not like '%USDA Commodity%'
+	) and
+	--------- PRESERVATIVES & ADDITIVES ---------
+	-- Currently, processed foods are given the benefit of the doubt. Only the best available ones are selected (without salt, with skin, etc), even though this is not what most consumers eat
+	(
 		long_desc not like '%with salt%' and
 		long_desc not like '%with added ascorbic acid%' and
 		long_desc not like '%with added sugar%' and
 		long_desc not like '%sulfured%' and
 		long_desc not like '%without skin%' and
 		long_desc not like '%drained%' and
+		long_desc not like '%added solution%'
+	) and
+	(
+		--------- SIMPLE ---------
+		LENGTH(long_desc) < 11 or --short named foods are hard to filter for in other metrics and can be popular
 
 		--------- MEATLESS ---------
-		long_desc like '%egetarian%' or --not sure if removing first letter (due to capitalization worries) is really neccessary
-		long_desc like '%egan%' or
-		long_desc like '%eatless%' or
-
-		--------- COOKING METHODOLOGY ---------
-		(--generally cooked
-			LENGTH(long_desc) <= 35 and
-			long_desc like '%cooked%' and
-			long_desc not like '%uncooked%'
+		(
+			long_desc like '%egetarian%' or --not sure if removing first letter (due to capitalization worries) is really neccessary
+			long_desc like '%egan%' or
+			long_desc like '%eatless%'
 		) or
-		(long_desc like '%home%' and long_desc like '%prepared%') or -- home prepped
-		(-- unique cooking types
-			long_desc like '%grilled%' or
-			long_desc like '%roasted%' or
-			long_desc like '%broiled%' or
-			long_desc like '%BBQ%'
+		--------- COOKING METHODOLOGY ---------
+		(
+			(--generally cooked
+				LENGTH(long_desc) <= 35 and
+				long_desc like '%cooked%' and
+				long_desc not like '%uncooked%'
+			) or
+			(long_desc like '%home%' and long_desc like '%prepared%') or -- home prepped
+			(-- unique cooking types
+				long_desc like '%grilled%' or
+				long_desc like '%roasted%' or
+				long_desc like '%broiled%' or
+				long_desc like '%BBQ%'
+			)
 		) or
 
 		--------- FOOD GROUPS ---------
