172a9f791e8c68eb79e2ec77df014a9fbe1f677a
James <jtronlabs@gmail.com>
2017-11-29 10:39:23 -0500

    removed the huge FDA data files. started working on SQL to create those files given the SR28 SQLite database. Further processing may be needed after SQL to remove near identical products i.e. 'Beef, flank, steak, separable lean and fat, trimmed to 0" fat, choice, cooked, broiled' vs 'Beef, flank, steak, separable lean and fat, trimmed to 0" fat, choice, cooked, braised'

---
+++ src/utils/fdaDataCreation/selections.sql
@@ -0,0 +1,30 @@
+-- select all foods
+SELECT food.id,food.long_desc,food_group.name FROM food
+	inner join food_group on food_group.id=food.food_group_id
+	where
+	food.food_group_id !=300 and -- baby food
+	food.food_group_id !=3500 and -- native food
+	food.food_group_id !=3600 and --restaruants
+	food.food_group_id != 1800 and --baked products
+	manufac_name is '' and --remove company specific food
+	(	--beef products:
+		food_group_id == 1300 and
+		long_desc like '%cooked%' and
+		long_desc not like '%by-products%' or
+		food_group_id != 1300
+	)
+	( --legumes
+		food_group_id==1600 and
+		long_desc like '%raw%' or
+		-- long_desc like '%cooked, boiled, without salt%' or
+		long_desc like '%flour%' or
+		long_desc like '%butter%' or
+		long_desc like '%tofu%' or
+		long_desc like '%meatless%' or
+		long_desc like '%yogurt%' or
+		long_desc like '%milk%' or
+		long_desc like '%egetarian%' or
+		long_desc like '%soybean%' or
+		long_desc == 'Papad' or
+		food_group_id != 1600
+	)
