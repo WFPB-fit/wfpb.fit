92de990bba406a7a9311ff7f5f052fca58a9abd3
James <jtronlabs@gmail.com>
2018-07-09 17:31:28 -0700

    ensres folders exist

--- offline_data_parsing/run_preprocess_data.py
+++ offline_data_parsing/run_preprocess_data.py
@@ -4,7 +4,9 @@ import os
 import shutil
 
 export_folder = "../src/assets/data/preprocessed_data/"
-shutil.rmtree(export_folder)
+if os.path.exists(export_folder):
+    shutil.rmtree(export_folder)
 os.makedirs(export_folder)
+
 fetch_foods.format_and_save_usda_food_data(export_folder)
 process_studies.preprocess_studies(export_folder)
