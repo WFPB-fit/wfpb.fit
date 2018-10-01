13aad3a7b5e58c8f1e2228d09ca5a192c6a62000
James <jtronlabs@gmail.com>
2018-10-01 12:43:18 -0700

    home page improvements

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -66,26 +66,16 @@ export default class Home extends Component {
 					<h2>What is WFPBD?</h2>
 
 					<p>
-						Diet high in a diverse range of nutrient-dense plant foods, and
-						avoids animal products and processed foods (such as oil, sugar, or
-						refined grains). WFPB is somewhat similar to the
-						{getLink(
-							"https://www.hsph.harvard.edu/nutritionsource/healthy-weight/diet-reviews/mediterranean-diet/",
-							"Mediterranean Diet"
-						)}
-						or the
-						{getLink(
-							"https://www.hsph.harvard.edu/nutritionsource/healthy-weight/diet-reviews/dash-diet/",
-							"DASH Diet"
-						)}
-						.
+						A diet high in a diverse range of nutrient-dense plant foods and low
+						in animal products or processed foods (such as oil, sugar, or
+						refined grains).
 					</p>
 
 					<Emojis>
 						<p>
 							✔️ - 🥕🍅🥒🌿🍆🍄🥔🌱🥦🌾🥝🍓🍋🍈🍊🍍🍐🍇🍉🍌🍒🥑🍎🥜🌰🌶️🌽🍠🍵
 						</p>
-						<p>❌ - 🥩🐖🐄🐔🦃🥚🥛🧀🐟🦐</p>
+						<p>❌ - 🥩🥚🥛🧀🐖🐄🐔🦃🐟🦐</p>
 						<p>❌ - 🍬🥐🍟🍦🥧🍺</p>
 					</Emojis>
 
@@ -105,8 +95,8 @@ export default class Home extends Component {
 					<p>
 						<b>
 							Disease is expensive, time consuming, miserable, and difficult.
-						</b>{" "}
-						WFPBD is a powerful way to help avoid disease.
+							WFPBD is a powerful way to help avoid disease.
+						</b>
 					</p>
 
 					<p>
@@ -125,77 +115,65 @@ export default class Home extends Component {
 					<ul>
 						<li>
 							<b>
-								Cholesterol is absorbed from animals and gets caught in our
-								arteries, eventually leading to Athleroscloris
-							</b>
-							. Athleroscloris can lead to heart disease and may play a role in
-							other chronic diseases. Plants do not contain cholesterol. Plants
-							contain phytosterols, which help reverse Athleroscloris.
-						</li>
-						<li>
-							<b>
-								Trans fat is found in animal meat and hydrogenated oils, and can
-								contribute to heart disease.
+								Trans fat, saturated fat, and to a lesser degree, dietary
+								cholesterol are primarily found in animal tissue and play a
+								strong role in artery and heart disease.
 							</b>{" "}
-							Plants do not contain trans fat.
+							Plants do not contain trans fat nor cholesterol, and saturated fat
+							is much less abundant. Plants also contain phytochemicals and
+							phytosterols that may help prevent artery disease. Artery disease
+							(Athleroscloris) may play a causal role in a variety of other
+							diseases beyond heart disease.
 						</li>
 						<li>
-							Whole plants contains fiber and resistant starch (RS). Poorly
-							digested by our bodies,{" "}
-							<b>fiber and RS feed the good bacteria in our gut</b>. Good
-							bacteria help feed and support our body's cells in the gut, and
-							crowd out bad bacteria. By feeding the bad bacteria with animal
-							products/processed foods, or{" "}
+							Poorly digested by our bodies,{" "}
 							<b>
-								by not feeding the good bacteria enough, our gut can let
-								disease-causing molecules into our body ("leaky gut syndrome").
-							</b>{" "}
-							This overactivates the immune system and can lead to inflammation,
-							autoimmune disease, and cancer.
-						</li>
-						<li>
-							<b>
-								Fiber and resistant starch is filling, and make it easier to
-								stop over-eating (increased satiety).{" "}
+								whole plant foods are the only source of fiber and resistant
+								starch (RS), which feed the "good" bacterial flora in our gut.
 							</b>
+							Without this protective flora, disease-causing molecules can enter
+							more freely into our body. This "leaky gut syndrome" overactivates
+							the immune system and may lead to chronic inflammation, autoimmune
+							disease and cancer.
 						</li>
 						<li>
-							Cells and their DNA can be damaged by Oxidative Stress. This can
-							lead to cancer and other diseases.{" "}
+							<b>
+								Fiber and resistant starch is filling (high satiety), and make
+								it easier to stop over-eating.{" "}
+							</b>
+							In addition to being critical to health and very filling, they
+							also provide little to no calories which helps prevent or reverse
+							obesity.
+						</li>
+						<li>
+							Cellular DNA can be damaged by Oxidative Stress, which can lead to
+							cancer.{" "}
 							<b>
 								Antioxidants help prevent oxidative stress, and are bountiful in
 								plants.
 							</b>{" "}
-							Antioxidants include vitamins and plant chemicals (phytochemicals)
-							like Carotenoids and Flavonoids.
+							Antioxidants include vitamins and phytochemicals like Carotenoids
+							and Flavonoids.
 						</li>
 						<li>
-							<b>
-								Animal products contain infectious disease causing organisms
-								(pathogens).
-							</b>{" "}
-							Food safety ensures pathogens do not grow, but mistakes can lead
-							to <b>food-borne illness.</b> Only when contaminated with animal
-							waste do plants lead to food-borne illness.
-						</li>
-						<li>
-							Animal agriculture is the #1 user of antibiotics, and even uses
-							"last-resort" antibiotics.{" "}
+							Animal agriculture is the #1 user of antibiotics. It has even been
+							caught using "last-resort" antibiotics.{" "}
 							<b>
 								Closely confined and medicated animals leads to the evolution of
 								new and antibiotic-resistent disease, some of which can be
-								spread to humans.
+								spread to humans through air, food, or water contamination from
+								waste ponds.
 							</b>
 						</li>
-						<li>And more...</li>
+						<li>{getLink("/research", "And more...")}</li>
 					</ul>
 
 					<h2>How can I be sure of these benefits?</h2>
 					<p>
 						Without expert training and a constant look into available research,{" "}
 						<b>it can be difficult to know what to believe. </b>
-						Studies can be poorly or dishonestly designed. Meta analysis can
-						summarize research, but bias and politics can warp their
+						Studies can be poorly or dishonestly designed. Reviews/meta analysis
+						can summarize research, but bias and politics can warp their
 						conclusions.
 					</p>
 					<p>
@@ -203,12 +181,13 @@ export default class Home extends Component {
 							WFPBD has a large body of evidence to support it as a healthy way
 							to eat.
 						</b>{" "}
-						Decades of diverse, quality research and the endorsements of
-						governments and physician groups support WFPBD.{" "}
-						<b>This site aggregates info and research into WFPBD.</b>
+						Decades of diverse, quality {getLink("/research", "research")}{" "}
+						support WFPBD. Governments, professional medical associations, and
+						many thousands of regular people{" "}
+						{getLink("/endorsements", "endorse")} WFPBD.
 					</p>
 
-					<h2>Nutrients to be careful of on WFPBD</h2>
+					<h2>Potential Nutrient Deficiencies on WFPBD</h2>
 					<ol>
 						<li>
 							Created by bacteria, <b>vitamin B12</b> is stored in animal
@@ -232,39 +211,38 @@ export default class Home extends Component {
 							foods, algae supplements, or a UVB lamp to get your vitamin D.
 						</li>
 					</ol>
-					<h3>Other Nutrients To Consider</h3>
+					<h3>Common Nutrition Concerns About WFPBD</h3>
 					<p>
 						Most people will get all other required nutrients when eating a
-						diverse range of plant foods. If you are worried about your
-						nutrition try tracking it with{" "}
-						{getLink("https://cronometer.com/", "Cronometer")} or a similar app.
+						diverse range of whole, plant-based foods. If you are worried about
+						your nutrition try tracking it with{" "}
+						{getLink("https://cronometer.com/", "Cronometer")}, a similar app,
+						or discussing with your doctor.
 					</p>
-					<p>People often have concerns about:</p>
-					
+
 					<ol>
 						<li>
 							Americans eat twice the <b>protein</b> that's recommended.{" "}
-							<b>Excess protein can cause disease</b>: weakened bones, kidney or
-							liver disease, and increased risk of heart disease and cancer. The{" "}
 							<b>
-								USDA recommends daily intake of 0.4 grams/pound of body weight.
+								Excess protein can weaken bones and plays a role in kidney,
+								liver, heart disease and cancer.
 							</b>{" "}
-							High protein plant foods include beans, peas, nuts, seeds, soy
-							products, and whole grains.{" "}
+							The USDA's daily recommendation is 0.4 grams/pound of body weight.
+							This can easily be met eating high protein plant foods such as
+							beans, peas, nuts, seeds, soy products, and whole grains.{" "}
 							<b>Complementary Proteins are an outdated idea</b>, as nearly any
-							whole plant food can fulfill essential amino acid requirements.
+							whole plant food will provide all needed amino acids when eating
+							enough to fulfill your calorie needs.
 						</li>
 						<li>
-							<b>Omega 3 fatty acids</b> have been shown to improve brain and
-							joint health. Current evidence does not support their role in
-							improving heart health. There are 3 types: ALA, DHA, and EPA.
-							Plants only make ALA. Your body can convert ALA to the more useful
-							DHA+EPA, but only in small amounts.{" "}
-							<b>
-								It can be good to supplement with DHA/EPA derived from algae
-							</b>
-							, fulfilling your daily requirement while avoiding the
-							environmental contamination found in fish.
+							Current evidence does not support <b>Omega 3 fatty acid's</b> role
+							in improving heart health, but it may be good for brain and joint
+							health. There are 3 types. Plants only make ALA, which is an
+							essential nutrient. We can convert a small percentage to DHA & EPA
+							but eating more may be beneficial. Fish are the primary source of
+							DHA+EPA, but typically have heavy pollutant contamination. Fish
+							get their Omega 3s from algae, which can be eaten or supplemented
+							directly as a cleaner source.
 						</li>
 						<li>
 							<b>Iron</b> is harder to absorb from plants (non-heme) than
@@ -279,15 +257,31 @@ export default class Home extends Component {
 					<h2>Getting Started Tips</h2>
 					<ol>
 						<li>
-							<b>Everyone's body is different.</b> Reducing animal
-							products/processed foods and increasing WFPB intake is generally
-							associated with better health. However, allergies, food
-							sensitivities, or digestion issues may make your individual diet
-							look a little different. Talk with a doctor or dietitian for help.
+							<b>Everyone's body is different.</b> Increasing WFPB intake and
+							reducing animal products+processed foods is generally associated
+							with better health. However, allergies, food sensitivities, or
+							digestion issues may make your individual diet look a little
+							different. Talk with a doctor or dietitian for help.
 						</li>
 						<li>
-							<b>Perfection is not required</b>. However, to get significant
-							health benefits <b>long-term consistency is</b>.
+							<b>
+								Perfection is not required, but "moderation in everything" is
+								not necessarily healthier.
+							</b>{" "}
+							For example, the very similar{" "}
+							{getLink(
+								"https://www.hsph.harvard.edu/nutritionsource/healthy-weight/diet-reviews/mediterranean-diet/",
+								"Mediterranean"
+							)}
+							or
+							{getLink(
+								"https://www.hsph.harvard.edu/nutritionsource/healthy-weight/diet-reviews/dash-diet/",
+								"DASH"
+							)}{" "}
+							diets have been shown to have very good health outcomes, with
+							amazingly huge improvements over a typical westerner's health and
+							diet. But when directly tested against WFPBD, WFPBD typically
+							produces even better health outcomes.
 						</li>
 						<li>
 							<b>
@@ -301,11 +295,9 @@ export default class Home extends Component {
 							, online communities, family, and friends.
 						</li>
 						<li>
-							Focusing on eating "good stuff" may be easier than trying to avoid
-							"bad stuff".{" "}
 							<b>
-								Filling up on healthy whole foods makes it less likely to
-								over-indulge in unhealthy snacks.
+								Filling up on healthy, whole, plant-based foods first makes it
+								less likely to over-indulge in unhealthy snacks.
 							</b>
 						</li>
 						<li>
@@ -316,11 +308,10 @@ export default class Home extends Component {
 							in as little as 2 months.
 						</li>
 						<li>
-							Eating low-calorie, nutritionally dense food may make you
-							calorie-deficient without realizing it.{" "}
 							<b>
-								You may need to increase the size/frequency of WFPB meals to
-								maintain your energy (calorie) intake.
+								Eating a nutrient-dense WFPBD may make it necessary to increase the
+								volume, amount, or frequency of food eaten to maintain your calorie
+								intake.
 							</b>
 						</li>
 						<li>
