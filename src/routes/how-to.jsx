a13d63b2be573c34d61c1ec0e008c38056d37d9c
James <jtronlabs@gmail.com>
2018-10-24 22:38:50 -0700

    removed the 'how does it work' section from how-to, i think it is better to let the primary sources speak for themselves+they are more accurate than I can be. Added more sites for finding wFPB doctors

--- src/routes/how-to.jsx
+++ src/routes/how-to.jsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { WidthWrapper, getLink } from "../utils/GeneralUtils.jsx";
 
 import ModalImage from "../components/ModalImage";
-import KeyValueTable from "../components/keyValueTable";
+// import KeyValueTable from "../components/keyValueTable";
 
 import QuickResources from "../assets/data/quick-resources.json";
 import Heading from "../components/heading";
@@ -56,6 +56,18 @@ export default class HowTo extends Component {
 					src="/imgs/assets/foods/pyramids/wfpb_food_pyramid_nutriciously.jpg"
 					alt="Nutriciously WFPB low fat food pyramid"
 				/>
+				<Heading id="help" txt="Where can I get help?" variant="h2" />
+				<Heading id="drs" txt="Doctors" variant="h3" />
+				Find a medical professional to help you on your journey.
+				{HowTo.getListed(QuickResources.doctors)}
+				<Heading id="communities" txt="Communities" variant="h3" />
+				Online forums are a great place for advice, questions, discussion, and
+				motivation.
+				{HowTo.getListed(QuickResources.communities)}
+				<Heading id="programs" txt="Programs" variant="h3" />
+				Structured programs can guide you through the transition. Some cost
+				money, some are free.
+				{HowTo.getListed(QuickResources.programs)}
 				<Heading id="learn" txt="Where can I learn more?" variant="h2" />
 				<p>
 					See the {getLink("/research", "research")} page or start looking
@@ -72,18 +84,10 @@ export default class HowTo extends Component {
 					{getLink("/endorsements", "endorsements")} page.{" "}
 				</p>
 				{HowTo.getListed(QuickResources.books)}
-				<Heading id="help" txt="Where can I get help?" variant="h2" />
-				<Heading id="communities" txt="Communities" variant="h3" />
-				Online forums are a great place for advice, questions, discussion, and
-				motivation.
-				{HowTo.getListed(QuickResources.communities)}
-				<Heading id="programs" txt="Programs" variant="h3" />
-				Structured programs can guide you through the transition. Some cost
-				money, some are free.
-				{HowTo.getListed(QuickResources.programs)}
 				<Heading id="recipes" txt="What's for dinner?" variant="h2" />
 				Whether you need cheap, fast, and easy or delicious and decadent there's
-				a WFPB recipe waiting for you.
+				a WFPB recipe waiting for you. Also see the "Books" section for recipe
+				books.
 				{HowTo.getListed(QuickResources.recipes)}
 				<Heading id="tips" txt="Getting Started Tips" variant="h2" />
 				<ol>
@@ -111,8 +115,12 @@ export default class HowTo extends Component {
 						)}{" "}
 						diets have been shown to have very good health outcomes, with
 						amazingly huge improvements over a typical westerner's health and
-						diet. But when directly tested against WFPBD, WFPBD typically
-						produces even better health outcomes.
+						diet. But when{" "}
+						{getLink(
+							"/research?selected=DASH_mediterranean_moderation_vegan_WFPB_vegetarian#food",
+							"direct tested against WFPB"
+						)}{" "}
+						, WFPBD typically produces even better health outcomes.
 					</li>
 					<li>
 						<b>
@@ -167,168 +175,6 @@ export default class HowTo extends Component {
 							"/imgs/assets/foods/produce-chart.jpg",
 							"properly storing produce."
 						)}
-					</li>
-				</ol>
-				<Heading id="how" txt="How does it work?" variant="h2" />
-				<ul>
-					<li>
-						Most Americans are{" "}
-						{getLink(
-							"https://www.ars.usda.gov/northeast-area/beltsville-md-bhnrc/beltsville-human-nutrition-research-center/food-surveys-research-group/docs/wweia-usual-intake-data-tables/",
-							"deficient in essential nutrients"
-						)}{" "}
-						that are common in a WFPB diet. Fixing these deficiencies can
-						greatly improve overall health.
-						<KeyValueTable
-							// title="Percentage of Americans Defiicent In..."
-							style={{ maxWidth: "350px", margin: "5px auto" }}
-							keyLabel="Nutrient"
-							valueLabel="Americans Deficient (%)"
-							data={{
-								Fiber: 97,
-								Potassium: 97,
-								"Vitamin E": 97,
-								"Vitamin K": 73,
-								Calcium: 70,
-								Magnesium: 56,
-								ALA: 44,
-								"Vitamin C": 31
-							}}
-						/>
-						A small number of nutrients may be{" "}
-						{getLink("/how-to#potential-issues", "less common", true)} in WFPBD
-						though, so plan your diet well to ensure proper nutrition.
-						{/* Nutrients that are {getLink("/how-to#potential-issues","less common",true)} in WFPBD are less likely to
-							have deficiencies.
-							<KeyValueTable
-								// title="Percentage of Americans Defiicent In..."
-								style={{ maxWidth: "350px", margin: "5px auto" }}
-								keyLabel="Nutrient"
-								valueLabel="Americans Deficient (%)"
-								data={{
-									"Vitamin D": 69,
-									Zinc: 12,
-									Iron: 5,
-									Protein: 3,
-									B12: 3
-								}}
-							/> */}
-					</li>
-					<li>
-						<b>
-							Trans fat, saturated fat, and to a lesser degree, dietary
-							cholesterol are primarily found in animal tissue and play a strong
-							role in artery and heart disease.
-						</b>{" "}
-						Plants do not contain trans fat nor cholesterol, and saturated fat
-						is much less abundant. Plants also contain phytochemicals and
-						phytosterols that may help prevent artery disease. Artery disease
-						(Athleroscloris) may play a causal role in a variety of other
-						diseases beyond heart disease.
-					</li>
-					<li>
-						Poorly digested by our bodies,{" "}
-						<b>
-							whole plant foods are the only source of fiber and resistant
-							starch (RS), which feed the "good" bacterial flora in our gut.
-						</b>
-						Without this protective flora, disease-causing molecules can enter
-						more freely into our body. This "leaky gut syndrome" overactivates
-						the immune system and may lead to chronic inflammation, autoimmune
-						disease and cancer.
-					</li>
-					<li>
-						<b>
-							Fiber and resistant starch is filling (high satiety), and make it
-							easier to stop over-eating.{" "}
-						</b>
-						In addition to being critical to health and very filling, they also
-						provide little to no calories which helps prevent or reverse
-						obesity.
-					</li>
-					<li>
-						Cellular DNA can be damaged by Oxidative Stress, which can lead to
-						cancer.{" "}
-						<b>
-							Antioxidants help prevent oxidative stress, and are bountiful in
-							plants.
-						</b>{" "}
-						Antioxidants include vitamins and phytochemicals like Carotenoids
-						and Flavonoids.
-					</li>
-					<li>
-						Animal agriculture is the #1 user of antibiotics. It has even been
-						caught using "last-resort" antibiotics.{" "}
-						<b>
-							Closely confined and medicated animals leads to the evolution of
-							new and antibiotic-resistent disease, some of which can be spread
-							to humans through air, food, or water contamination from waste
-							ponds.
-						</b>
-					</li>
-					<li>{getLink("/research", "And more...")}</li>
-				</ul>
-				<Heading id="potential-issues" txt="Potential Issues" variant="h2" />
-				The following nutrients might be harder to get on WFPBD.
-				<ol>
-					<li>
-						Created by bacteria, <b>vitamin B12</b> is stored in animal tissue.
-						Without eating animals B12 will need to be taken as a vitamin or
-						eaten through fortified foods. B12 supplementation can be helpful
-						even on regular diets, as
-						{getLink(
-							"https://www.plantbaseddoctors.org/",
-							"3% are deficient, 7% of seniors, and 14-17% of adults are marginally depleted."
-						)}
-					</li>
-					<li>
-						<b>Vitamin D</b> is created when our skin is exposed to sunlight.
-						{getLink(
-							"https://ods.od.nih.gov/factsheets/VitaminD-HealthProfessional/",
-							"5-30 minutes of daytime sunlight, twice a week fulfills our requirement for Vitamin D."
-						)}
-						When there's not enough sun, Vitamin D can be obtained through a
-						multi-vitamin (cheap and effectve), fortified foods, algae
-						supplements, or a special UVB lamp.
-					</li>
-				</ol>
-				<Heading id="common-concerns" txt="Common Concerns" variant="h3" />
-				<p>
-					Most people will get all other required nutrients when eating a
-					diverse range of whole, plant-based foods. If you are worried about
-					your nutrition try tracking it with{" "}
-					{getLink("https://cronometer.com/", "Cronometer,")} a similar app,
-					discussing with your doctor, or getting a blood test.
-				</p>
-				<ol>
-					<li>
-						Americans eat twice the <b>protein</b> that's recommended.{" "}
-						<b>
-							Excess protein can weaken bones and plays a role in kidney, liver,
-							heart disease and cancer.
-						</b>{" "}
-						The USDA's daily recommendation is 0.4 grams/pound of body weight.
-						This can easily be met eating high protein plant foods such as
-						beans, peas, nuts, seeds, soy products, and whole grains.{" "}
-						<b>Complementary Proteins are an outdated idea</b>, as nearly any
-						whole plant food will provide all needed amino acids when eating
-						enough to fulfill your calorie needs.
-					</li>
-					<li>
-						Current evidence does not support <b>Omega 3 fatty acid's</b> role
-						in improving heart health, but it may be good for brain and joint
-						health. There are 3 types. Plants only make ALA, which is an
-						essential nutrient. We can convert a small percentage to DHA & EPA
-						but eating more may be beneficial. Fish are the primary source of
-						DHA+EPA, but typically have heavy pollutant contamination. Fish get
-						their Omega 3s from algae, which can be eaten or supplemented
-						directly as a cleaner source.
-					</li>
-					<li>
-						<b>Iron, Calcium, and Zinc</b> are common concerns but deficiencies
-						are rare with WFPBD. Eating diverse, fresh plant foods will ensure
-						you get more than enough, particularly leafy greens, beans, and
-						seeds are great sources.
 					</li>
 				</ol>
 			</WidthWrapper>
