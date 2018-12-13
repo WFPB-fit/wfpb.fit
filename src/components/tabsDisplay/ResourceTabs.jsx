d705cbe1380e504c20e6e6796df39e170879b541
James <jtronlabs@gmail.com>
2018-12-12 18:12:21 -0800

    refactor

--- src/components/tabsDisplay/ResourceTabs.jsx
+++ src/components/tabsDisplay/ResourceTabs.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import Resources from "../resources/index.jsx";
 
-import { withRouter } from "react-router-dom";
 import LinkableTabs from "./LinkableTabs.jsx";
 import Typography from "@material-ui/core/Typography";
 
 export default class ResourceTabs extends Component {
 	render() {
-		// const NonBlockedResources = withRouter(Resources);
-
 		const tabData = this.props.tabs.map(x => {
 			let content = x.component;
 			if (!content) {
